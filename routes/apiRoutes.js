3c104dbbd915f5f22953061a6728f13a3a802b47
SchneiderMan <jschneid94@gmail.com>
2018-09-17 18:27:48 -0700

    HTML routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,6 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
 
 // API ROUTING OVERVIEW
   // Welcome page - Main landing page for forum
@@ -91,3 +70,4 @@ module.exports = function(app) {
             res.json(dbPost);
           });
       });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,45 +1,21 @@
-var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
-  // Load index page
+  // HTML ROUTE OVERVIEW
+  // Welcome page - Main landing page for forum
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Category page - Page listing all posts for a specific category (ex. HTML category)
+  app.get("/category", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/category.html"));
   });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // Individual Post page - Page with original post and all comments
+  app.get("/post", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/post.html"));
+  });
+  // Create Post page - Page to create a forum post
+  app.get("/create", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/create.html"));
   });
 };
-
-// HTML ROUTE OVERVIEW
-  // Welcome page - Main landing page for forum
-    app.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/blog.html"));
-    });
-  // Category page - Page listing all posts for a specific category (ex. HTML category)
-    app.get("/category", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/cms.html"));
-    });
-  // Individual Post page - Page with original post and all comments
-    app.get("/post", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/blog.html"));
-    });
-  // Create Post page - Page to create a forum post
-    app.get("/create", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/blog.html"));
-    });
