e655f944d6fc70be986914449afa98ec9384af5a
Roger <rdpender3@gmail.com>
2018-09-19 20:13:01 -0700

    worked on user route

--- routes/users-api-routes.js
+++ routes/users-api-routes.js
@@ -1,18 +1,18 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/users", function(req, res) {
+  app.get("/api/userTable", function(req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
     db.Users.findAll({
-    //   include: [db.Topics]
+      include: [db.Posts, dp.Replies]
     }).then(function(dbUsers) {
       res.json(dbUsers);
     });
   });
 
-  app.get("/api/users/:id", function(req, res) {
+  app.get("/api/userTable/:id", function(req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
@@ -20,19 +20,19 @@ module.exports = function(app) {
       where: {
         id: req.params.id
       },
-      include: [db.Topics]
+      include: [db.Posts, db.Replies]
     }).then(function(dbUsers) {
       res.json(dbUsers);
     });
   });
 
-  app.post("/api/users", function(req, res) {
+  app.post("/api/userTable", function(req, res) {
     db.Users.create(req.body).then(function(dbUsers) {
       res.json(dbUsers);
     });
   });
 
-  app.delete("/api/users/:id", function(req, res) {
+  app.delete("/api/userTable/:id", function(req, res) {
     db.Users.destroy({
       where: {
         id: req.params.id
