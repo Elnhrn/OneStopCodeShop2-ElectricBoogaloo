cc6ac792a164b46bafb9e045c9144a10dea1612b
SchneiderMan <jschneid94@gmail.com>
2018-09-19 20:20:19 -0700

    Setup node modules in server.js

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ var PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  expressSession({
+    secret: "butthole",
+    saveUninitialized: false,
+    resave: false
+  })
+);
 
 // Handlebars
 app.engine(
