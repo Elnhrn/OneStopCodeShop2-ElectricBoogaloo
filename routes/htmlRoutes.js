786aaf90b61a5b9aa19e113c8523cde97e531528
SchneiderMan <jschneid94@gmail.com>
2018-09-20 16:25:56 -0700

    Added separate pages and routes for login and creating account

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,13 +3,43 @@ var path = require("path");
 module.exports = function(app) {
   // HTML ROUTE OVERVIEW
   // Welcome page - Main landing page for forum
-  app.get("/", function(req, res) {
+  app.get("/login", function(req, res) {
     res.render("login", {
       title: "Form Validation",
       success: req.session.success,
       errors: req.session.errors
     });
-    // res.redirect("/login");
+    req.session.errors = null;
+  });
+
+  app.post("/login", function(req, res) {
+    req.check("email", "Invalid email address").isEmail();
+    req
+      .check("password", "Password is invalid")
+      .isLength({
+        min: 4
+      })
+      .equals(req.body.confirmPassword);
+    var errors = req.validationErrors();
+    if (errors) {
+      req.session.errors = errors;
+      req.session.success = false;
+    } else {
+      req.session.success = true;
+    }
+    res.redirect("/");
+  });
+
+  app.get("/", function(req, res) {
+    if (req.session.success) {
+      res.render("index");
+    } else {
+      res.render("login");
+    }
+  });
+
+  app.get("/createAccount", function(req, res) {
+    res.render("createAccount");
   });
 
   app.post("/submit", function(req, res) {
@@ -30,9 +60,6 @@ module.exports = function(app) {
     res.redirect("/");
   });
 
-  app.get("/login", function(req, res) {
-    res.render("login");
-  });
   // Category page - Page listing all posts for a specific category (ex. HTML category)
   app.get("/category", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/category.html"));
