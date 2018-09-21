52ccd28224b43e6b679ec0f11793d0cf7d77f194
Roger <rdpender3@gmail.com>
2018-09-20 18:34:21 -0700

    added some routing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,44 +1,67 @@
+// 1. home page
+// 2. login/register
+// 3. my account
+// 4. by category/topic
+// 5. by author
+// 6. by post
+// 7. create post
+
 var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbForum) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbForum
+    res.render("index", {
+      msg: "Welcome to the electric boogaloo!"
+    });
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/login", function(req, res) {
+    res.render("login/index", {
+      msg: "Welcome back/Create new?"
+    });
+  });
+
+  app.get("/account", function(req, res) {
+    db.Users.findOne({}).then(function(dbUsers) {
+      res.render("myAccount/index", {
+        users: dbUsers
       });
     });
   });
 
-  // THIS WORKS TOO!!!!!!!!!!!
-  // Load example page and pass in an example by id
-  // app.get("/forum", function(req, res) {
-  //   db.Forum.findAll({}).then(function(dbForum) {
-  //     res.render("example", {
-  //       forum: dbPosts
-  //     });
-  //   });
-  // });
+  app.get("/topics", function(req, res) {
+    db.Topics.findAll({}).then(function(dbTopics) {
+      res.render("topics/index", {
+        topics: dbTopics
+      });
+    });
+  });
 
-  // app.get("/forum", function(req, res) {
-  //   db.Topics.findAll({}).then(function(dbTopics) {
-  //     res.render("example", {
-  //       forum: dbTopics
-  //     });
-  //   });
-  // });
+  app.get("/author", function(req, res) {
+    db.Posts.findAll({}).then(function(dbPosts) {
+      res.render("author/index", {
+        author: dbPosts
+      });
+    });
+  });
 
-  // THIS WORKS
-  // app.get("/forum", function(req, res) {
-  //   db.Users.findAll({}).then(function(dbUsers) {
-  //     console.log("did i run?")
-  //     console.log(dbUsers)
-  //     res.render("example", {
-  //       forum: dbUsers
-  //     });
-  //   });
-  // });
+  app.get("/posts", function(req, res) {
+    db.Posts.findAll({}).then(function(dbPosts) {
+      res.render("posts/index", {
+        posts: dbPosts
+      });
+    });
+  });
+
+  app.get("/add-a-post", function(req, res) {
+    db.Posts.create({}).then(function(dbPosts) {
+      res.render("createPost/index", {
+        newPost: dbPosts
+      });
+    });
+  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
