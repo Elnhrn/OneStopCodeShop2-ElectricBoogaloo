0f2e52d5bc9d5123daffe757785ca79559d92266
Roger <rdpender3@gmail.com>
2018-09-24 16:12:26 -0700

    working on some stuff

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,38 @@
-var db = require("../models");
+//DOESN'T CURRENTLY WORK. SHOULD BE ABLE TO REWORK IN CLASS TOMORROW
 
-$(document).ready(function () {
+// var db = require("../models");
 
-    // initializing the select options
-    $('select').formSelect();
+// $(document).ready(function () {
 
-    $('#submit').on('click', function (event) {
+//     // initializing the select options
+//     $('select').formSelect();
 
-        // add a validation instead of prevent default?
+//     $('#submit').on('click', function (event) {
 
-        event.preventDefault();
+//         event.preventDefault();
 
-        // pulling values from the survey and storing them in an object
-        let postTitle = $("#post_title").val().trim();
-        let topic = $("#topic_name").val().trim();
-        let postBody = $("#textarea1").val();
+//         // pulling values from the survey and storing them in an object
+//         let postTitle = $("#post_title").val().trim();
+//         let topic = $("#topic_name").val().trim();
+//         let postBody = $("#textarea1").val();
 
-        let addToTopic= topic_number + 1;
+//         let addToTopic= topic_number + 1;
 
-        db.Topics.update({ where: { topic_name: topic}, addToTopic}).then(function (dbTopics) {
-            db.Posts.create({
-                post_subject: postTitle,
-                post_body: postBody,
-                post_rating: 0,
-                post_number: 0,
-                // NEED TO FIND WAY TO REFERENCE THESE
-                // UserId: ,
-                TopicId: dbTopics.id
-            }).then({
+//         db.Topics.update({ where: { topic_name: topic}, addToTopic}).then(function (dbTopics) {
+//             db.Posts.create({
+//                 post_subject: postTitle,
+//                 post_body: postBody,
+//                 post_rating: 0,
+//                 post_number: 0,
+//                 // NEED TO FIND WAY TO REFERENCE THESE
+//                 // UserId: ,
+//                 TopicId: dbTopics.id
+//             }).then({
 
-            });
+//             });
 
-        });
+//         });
 
-    });
+//     });
 
-});
+// });
