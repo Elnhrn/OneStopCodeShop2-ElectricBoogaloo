ce8f91c4417521d5b66a84eabac9b4b14393f25c
Roger <rdpender3@gmail.com>
2018-09-23 22:02:25 -0700

    got some stuff done

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -78,7 +78,8 @@ module.exports = function (app) {
   app.get("/forum", function (req, res) {
     if (req.session.success) {
       db.Topics.findAll({}).then(function (dbTopics) {
-        db.Posts.findAll({ order: ["post_rating", "ASC" ] }).then(function (dbPosts) {
+        // COME BACK TO THIS
+        db.Posts.findAll({ order: [["post_rating", "ASC"]], limit: 5 }).then(function (dbPosts) {
           res.render("forum/index", {
             msg: "Welcome to the forum!",
             topics: dbTopics,
@@ -111,10 +112,14 @@ module.exports = function (app) {
     if (req.session.success) {
       db.Topics.findOne({ where: { id: req.params.id } }).then(function (dbTopics) {
         db.Posts.findAll({ where: { TopicID: req.params.id } }).then(function (dbPosts) {
-          res.render("topics/index", {
-            topics: dbTopics,
-            posts: dbPosts,
-            success: req.session.success
+          // COME BACK TO THIS
+          db.Posts.findAll({ order: [["post_rating", "ASC"]], limit: 5 }).then(function (dbPosts2) {
+            res.render("topics/index", {
+              topics: dbTopics,
+              posts2: dbPosts2,
+              posts: dbPosts,
+              success: req.session.success
+            });
           });
         });
       });
@@ -144,16 +149,40 @@ module.exports = function (app) {
 
   app.get("/posts", function (req, res) {
     if (req.session.success) {
-      db.Posts.findAll({}).then(function (dbPosts) {
-        res.render("posts/index", {
-          posts: dbPosts,
-          success: req.session.success
+      db.Posts.findAll({}).then(function (dbPosts2) {
+        // COME BACK TO THIS
+        db.Posts.findAll({ order: [["post_rating", "ASC"]], limit: 5 }).then(function (dbPosts) {
+          res.render("posts/index", {
+            posts2: dbPosts2,
+            posts: dbPosts,
+            success: req.session.success
+          });
         });
       });
     } else {
       res.redirect("/login");
     }
   });
+
+  app.get("/posts/:id", function (req, res) {
+    if (req.session.success) {
+      db.Posts.findOne({ where: { id: req.params.id } }).then(function (dbPosts2) {
+        db.Replies.findAll({ where: { PostId: req.params.id } }).then(function (dbReplies) {
+          // COME BACK TO THIS
+          db.Posts.findAll({ order: [["post_rating", "ASC"]], limit: 5 }).then(function (dbPosts) {
+            res.render("this-post/index", {
+              posts2: dbPosts2,
+              posts: dbPosts,
+              replies: dbReplies,
+              success: req.session.success
+            });
+          });
+        });
+      });
+    } else {
+      res.redirect("/login");
+    }
+  })
 
   app.get("/add-a-post", function (req, res) {
     if (req.session.success) {
