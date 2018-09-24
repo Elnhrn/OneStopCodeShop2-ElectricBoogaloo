ef742c33439ca6d9a6d6314214bd67e68b7e8c32
Roger <rdpender3@gmail.com>
2018-09-24 16:03:10 -0700

    working on javascript

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+var db = require("../models");
+
+$(document).ready(function () {
+
+    // initializing the select options
+    $('select').formSelect();
+
+    $('#submit').on('click', function (event) {
+
+        // add a validation instead of prevent default?
+
+        event.preventDefault();
+
+        // pulling values from the survey and storing them in an object
+        let postTitle = $("#post_title").val().trim();
+        let topic = $("#topic_name").val().trim();
+        let postBody = $("#textarea1").val();
+
+        let addToTopic= topic_number + 1;
+
+        db.Topics.update({ where: { topic_name: topic}, addToTopic}).then(function (dbTopics) {
+            db.Posts.create({
+                post_subject: postTitle,
+                post_body: postBody,
+                post_rating: 0,
+                post_number: 0,
+                // NEED TO FIND WAY TO REFERENCE THESE
+                // UserId: ,
+                TopicId: dbTopics.id
+            }).then({
+
+            });
+
+        });
+
+    });
+
+});
