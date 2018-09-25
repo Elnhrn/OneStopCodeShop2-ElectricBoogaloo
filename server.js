fb6de29c79daadb1d0ed61b9d952241b6d639f56
elaine <user@host.com>
2018-09-24 21:34:44 -0700

    stylizing post pages

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(expressValidator());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(
   expressSession({
     secret: "JRS",
