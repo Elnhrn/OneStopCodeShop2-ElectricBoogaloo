13a26cb0ff97f697b17b7d9fb076d409bf74391c
Roger <rdpender3@gmail.com>
2018-09-24 21:42:59 -0700

    trying to get submit to work

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,40 @@
-//DOESN'T CURRENTLY WORK. SHOULD BE ABLE TO REWORK IN CLASS TOMORROW
+// DOESN'T CURRENTLY WORK. SHOULD BE ABLE TO REWORK IN CLASS TOMORROW
 
 // var db = require("../models");
 
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//     // initializing the select options
-//     $('select').formSelect();
+    // initializing the select options
+    $('select').formSelect();
 
-//     $('#submit').on('click', function (event) {
+    $('#submit').on('click', function (event) {
 
-//         event.preventDefault();
+        event.preventDefault();
 
-//         // pulling values from the survey and storing them in an object
-//         let postTitle = $("#post_title").val().trim();
-//         let topic = $("#topic_name").val().trim();
-//         let postBody = $("#textarea1").val();
+        // pulling values from the survey and storing them in an object
+        let postTitle = $("#post_title").val().trim();
+        let topic = $("#topic_name").val().trim();
+        let postBody = $("#textarea1").val();
 
-//         let addToTopic= topic_number + 1;
+        console.log(postTitle, topic, postBody)
 
-//         db.Topics.update({ where: { topic_name: topic}, addToTopic}).then(function (dbTopics) {
-//             db.Posts.create({
-//                 post_subject: postTitle,
-//                 post_body: postBody,
-//                 post_rating: 0,
-//                 post_number: 0,
-//                 // NEED TO FIND WAY TO REFERENCE THESE
-//                 // UserId: ,
-//                 TopicId: dbTopics.id
-//             }).then({
+        // let addToTopic= topic_number + 1;
 
-//             });
+        // db.Topics.update({ where: { topic_name: topic}, addToTopic}).then(function (dbTopics) {
+            Posts.create({
+                post_subject: postTitle,
+                post_body: postBody,
+                post_rating: 0,
+                post_number: 0,
+                // NEED TO FIND WAY TO REFERENCE THESE
+                // UserId: ,
+                TopicId: dbTopics.id
+            }).then({
 
-//         });
+            });
 
-//     });
+        // });
 
-// });
+    });
+
+});
