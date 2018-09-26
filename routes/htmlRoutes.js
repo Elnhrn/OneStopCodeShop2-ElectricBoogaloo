34e9e46f6d0c9d9ae7ff4b19588dce1799f01bec
Roger <rdpender3@gmail.com>
2018-09-25 21:40:09 -0700

    commit before merge

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,8 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.render("index", {
       msg: "Welcome to the electric boogaloo!",
       success: req.session.success,
@@ -10,11 +10,11 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/forum", function(req, res) {
+  app.get("/forum", function (req, res) {
     if (req.session.success) {
-      db.Topics.findAll({}).then(function(dbTopics) {
-        // COME BACK TO THIS
-        db.Posts.findAll({ order: [["post_rating", "ASC"]], limit: 5 }).then(function (dbPosts) {
+      // COME BACK TO THIS
+      db.Posts.findAll({ order: [["post_rating", "ASC"]], limit: 5 }).then(function (dbPosts) {
+        db.Topics.findAll({}).then(function (dbTopics) {
           res.render("forum/index", {
             currentUser: req.session.user,
             msg: "Welcome to the forum!",
@@ -30,11 +30,11 @@ module.exports = function(app) {
     }
   });
 
-  app.get("/account/:id", function(req, res) {
+  app.get("/account/:id", function (req, res) {
     if (req.session.success) {
-      db.Users.findOne({ where: { id: req.params.id } }).then(function(dbUsers) {
-        db.Posts.findAll({ where: { UserId: req.params.id } }).then(function(dbPosts) {
-          db.Replies.findAll({ where: { UserId: req.params.id } }).then(function(dbReplies) {
+      db.Users.findOne({ where: { id: req.params.id } }).then(function (dbUsers) {
+        db.Posts.findAll({ where: { UserId: req.params.id } }).then(function (dbPosts) {
+          db.Replies.findAll({ where: { UserId: req.params.id } }).then(function (dbReplies) {
             res.render("myAccount/index", {
               currentUser: req.session.user,
               users: dbUsers,
@@ -51,44 +51,44 @@ module.exports = function(app) {
     }
   });
 
-  app.get("/topics/:id", function(req, res) {
+  app.get("/topics/:id", function (req, res) {
     if (req.session.success) {
-      db.Topics.findOne({ where: { id: req.params.id } }).then(function(
-        dbTopics
-      ) {
-        db.Posts.findAll({ where: { TopicID: req.params.id } }).then(function(
-          dbPosts
-        ) {
-          // COME BACK TO THIS
-          db.Posts.findAll({ order: [["post_rating", "ASC"]], limit: 5 }).then(
-            function(dbPosts2) {
+      // COME BACK TO THIS
+      db.Posts.findAll({ order: [["post_number", "ASC"]], limit: 5 }).then(
+        function (dbPosts) {
+          db.Topics.findOne({ where: { id: req.params.id } }).then(function (
+            dbTopics
+          ) {
+            db.Posts.findAll({ where: { TopicID: req.params.id } }).then(function (
+              dbPosts2
+            ) {
               res.render("topics/index", {
                 currentUser: req.session.user,
                 title: dbTopics.topic_name,
-                topics: dbTopics,
                 posts2: dbPosts2,
+                topics: dbTopics,
                 posts: dbPosts,
                 success: req.session.success
               });
             }
-          );
+            );
+          });
         });
-      });
     } else {
       res.redirect("/login");
     }
   });
 
-  app.get("/users/:id", function(req, res) {
+  app.get("/users/:id", function (req, res) {
     if (req.session.success) {
-      db.Users.findOne({ where: { id: req.params.id } }).then(function(
+      db.Users.findOne({ where: { id: req.params.id } }).then(function (
         dbUsers
       ) {
-        db.Posts.findAll({ where: { UserId: req.params.id } }).then(function(
+        db.Posts.findAll({ where: { UserId: req.params.id } }).then(function (
           dbPosts
         ) {
           db.Replies.findAll({ where: { UserId: req.params.id } }).then(
-            function(dbReplies) {
+            function (dbReplies) {
               res.render("author/index", {
                 currentUser: req.session.user,
                 title: dbUsers.user_name,
@@ -106,17 +106,18 @@ module.exports = function(app) {
     }
   });
 
-  app.get("/posts/:id", function(req, res) {
+  app.get("/posts/:id", function (req, res) {
     if (req.session.success) {
-      db.Posts.findOne({ where: { id: req.params.id } }).then(function(
-        dbPosts2
-      ) {
-        db.Replies.findAll({ where: { PostId: req.params.id } }).then(function(
-          dbReplies
-        ) {
-          // COME BACK TO THIS
-          db.Posts.findAll({ order: [["post_rating", "ASC"]], limit: 5 }).then(
-            function(dbPosts) {
+      // COME BACK TO THIS
+      db.Posts.findAll({ order: [["post_rating", "ASC"]], limit: 5 }).then(
+        function (dbPosts) {
+          db.Posts.findOne({ where: { id: req.params.id } }).then(function (
+            dbPosts2
+          ) {
+            db.Replies.findAll({ where: { PostId: req.params.id } }).then(function (
+              dbReplies
+            ) {
+
               res.render("this-post/index", {
                 currentUser: req.session.user,
                 title: dbPosts2.post_subject,
@@ -126,30 +127,31 @@ module.exports = function(app) {
                 success: req.session.success
               });
             }
-          );
+            );
+          });
         });
-      });
     } else {
       res.redirect("/login");
     }
   });
 
-  app.post("/posts/:id", function(req, res) {
+  app.post("/posts/:id", function (req, res) {
 
     let currentUser = req.session.user;
+    let postId = req.params.id
 
     // NEED TO REFERENCE POST ID
     db.Replies.create({
       reply_content: req.body.replyBody,
       reply_rating: 0,
-      PostId: 2,
+      PostId: postId,
       UserId: currentUser.id
     }).then(function (result) {
       res.redirect("/posts/" + result.PostId)
-  })
-});
+    })
+  });
 
-  app.get("/add-a-post", function(req, res) {
+  app.get("/add-a-post", function (req, res) {
     if (req.session.success) {
       // COME BACK TO THIS
       db.Posts.findAll({ order: [["post_rating", "ASC"]], limit: 5 }).then(function (dbPosts) {
